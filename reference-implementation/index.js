87ce21476e495faaff90e3db52b9b54acc411d29
webreflection <andrea.giammarchi@gmail.com>
2016-01-21 09:44:55 +0000

    using ? instead of ReturnIfAbrupt

--- reference-implementation/index.js
+++ reference-implementation/index.js
@@ -17,17 +17,15 @@ Object.defineProperty(
     writable: true,
     value: function getOwnPropertyDescriptors(genericObject) {
       let keys;
-      // Let obj be ToObject(O)
+      // Let obj be ?ToObject(O)
       if (Object(genericObject) !== genericObject) {
-        // ReturnIfAbrupt(obj)
         throw new Error('Argument should be an object');
       }
       try {
-        // Let keys be the result of calling the [[OwnPropertyKeys]]
+        // Let keys be the result of calling the ?[[OwnPropertyKeys]]
         // internal method of obj.
         keys = Reflect.ownKeys(genericObject);
       } catch(e) {
-        // ReturnIfAbrupt(keys)
         throw new Error('Unable to retrieve own keys');
       }
       // Let descriptors be the result of the abstract operation ObjectCreate
@@ -37,25 +35,22 @@ Object.defineProperty(
       let gotAllNames = keys.length === 0;
       // Repeat while gotAllNames is false,
       while (gotAllNames === false) {
-        // Let nextKey be IteratorValue(next).
-        // Let nextKey be ToPropertyKey(nextKey).
+        // Let nextKey be ?IteratorValue(next).
+        // Let nextKey be ?ToPropertyKey(nextKey).
         let nextKey = keys.shift();
         // If next is false, then let gotAllNames be true.
         gotAllNames = keys.length === 0;
         if (nextKey === undefined) {
-          // ReturnIfAbrupt(nextKey).
           throw new Error('Unable to iterate over own keys');
         }
-        // Let desc be the result of calling the [[GetOwnProperty]] internal method of obj with argument nextKey.
+        // Let desc be the result of calling the ?[[GetOwnProperty]] internal method of obj with argument nextKey.
         // Let descriptor be FromPropertyDescriptor(desc).
         let descriptor = Object.getOwnPropertyDescriptor(genericObject, nextKey);
         if (descriptor === undefined) {
-          // ReturnIfAbrupt(descriptor).
           throw new Error('Unable to retrieve own property descriptor');
         }
-        // Let status be the result of CreateDataProperty(descriptors, nextKey, descriptor).
+        // Let status be the result of ?CreateDataProperty(descriptors, nextKey, descriptor).
         try {
-          // Assert: status is not an abrupt completion.
           descriptors[nextKey] = descriptor;
         } catch(e) {
           throw new Error('Unable to create descriptors');
