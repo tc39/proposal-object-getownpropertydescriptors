1cb0e0bd9993216e273b743d27f3618889bce366
Jordan Harband <ljharb@gmail.com>
2016-01-26 23:47:46 -0800

    Clean up build process.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -e # exit with nonzero exit code if anything fails
 
-# clear and re-create the out directory
-rm -rf out || exit 0;
-mkdir out;
-
 # run our compile script
-node_modules/ecmarkup/bin/ecmarkup.js spec.html out/index.html
+npm run build
 
 # grab content
 INDEX=$(cat out/index.html)
